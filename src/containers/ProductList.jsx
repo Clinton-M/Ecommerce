267c0f926bb181df3dae0a868f1888e8f8508388
Clinton Mejia <107317012+Clinton-M@users.noreply.github.com>
2022-12-29 19:54:08 -0500

    Update ProductList.jsx

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,13 +1,26 @@
 import React,{useEffect, useState} from 'react'
 import '../styles/ProductList.scss'
 import ProductItem from '../components/ProductItem'
-import useGetProducts from '../hooks/useGetProducts';
+import axios from 'axios'
+import fetch from "node-fetch";
+import {json} from "react-router-dom";
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-    const products = useGetProducts(API);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () =>{
+            const response = await fetch(API);
+            const data = await response.json()
+            setProducts(data);
+        }
     
+        getProducts();
+
+        }, []);
+
     return (
         <section className="main-container">
             <div className="ProductList">
